0bc4544c2036e2d356343401a78c28a3070d97ee
joeunahh <jsj002251@gmail.com>
2024-05-27 19:40:29 +0900

    jojo(0527/19:40)

--- src/main/java/graduation/project/Controller/BankController.java
+++ src/main/java/graduation/project/Controller/BankController.java
@@ -1,5 +1,17 @@
 package graduation.project.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class BankController {
+	
+	@RequestMapping("bank/")
+	public String bankMain() {
+		return "bankMain";
+	}
+	
+	
 
 }
