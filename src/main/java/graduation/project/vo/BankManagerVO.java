e563f48280e84ad0eaf8d17d76941df5ad1eae3b
LYU-Joonseo <ljsh0706@naver.com>
2024-05-28 15:44:03 +0900

    joon(24.05.28/15:44)

--- src/main/java/graduation/project/vo/BankManagerVO.java
+++ src/main/java/graduation/project/vo/BankManagerVO.java
@@ -2,5 +2,31 @@ package graduation.project.vo;
 
 public class BankManagerVO {
 	
-
+	private String id;
+	private String pwd;
+	public BankManagerVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public BankManagerVO(String id, String pwd) {
+		super();
+		this.id = id;
+		this.pwd = pwd;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	@Override
+	public String toString() {
+		return "BankManagerVO [id=" + id + ", pwd=" + pwd + "]";
+	}
 }
